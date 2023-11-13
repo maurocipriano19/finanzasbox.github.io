c9aa6f7bd4a6485a5cc58855416ca1e89746e5ce
maurocipriano19 <104109893+maurocipriano19@users.noreply.github.com>
2023-11-13 16:07:17 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,26 +9,6 @@ function add(product, price) {
 }
 
 function pay() {
-    window.alert(products.join(", \n"));
-}
-
-//-----
-function displayProducts(productList) {
-    let productsHTML = '';
-    productList.forEach(element => {
-        productsHTML +=
-        `<div class="product-container">
-            <h3>${element.name}</h3>
-            <img src="${element.image}" />
-            <h1>$${element.price}</h1>
-            <button class="button-add" onclick="add(${element.id}, ${element.price})">Agregar</button>
-        </div>`
-    });
-    document.getElementById('page-content').innerHTML = productsHTML;
-}
-
-window.onload = async() => {
-    const productList = await (await fetch("/api/products")).json();
-    console.log(productList);
-    displayProducts(productList);
+    //window.alert(products.join(", \n"));
+    location.href="https://mpago.la/1JKXDw7"
 }
