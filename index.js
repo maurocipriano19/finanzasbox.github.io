23361865b077ed89f93c2231afe95edacc859c96
maurocipriano19 <104109893+maurocipriano19@users.noreply.github.com>
2023-11-13 15:54:45 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,45 +1,53 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-let products = [
+const products = [
   {
     id: 1,
-    name: "Finanzas desde cero",
-    price: 5499,
+    name: "2 Mancuernas",
+    price: 50,
     image: "images/product-1.jpg",
-    stock: 1,
+    stock: 50,
   },
   {
     id: 2,
-    name: "Analisis Técnico",
-    price: 5499,
+    name: "Banco ajustable",
+    price: 50,
     image: "images/product-2.jpg",
-    stock: 1,
+    stock: 50,
   },
-  
+  {
+    id: 3,
+    name: "Kit de pesas con estuche",
+    price: 50,
+    image: "images/product-3.jpg",
+    stock: 50,
+  },
+  {
+    id: 4,
+    name: "Pesas",
+    price: 50,
+    image: "images/product-4.jpg",
+    stock: 50,
+  },
+  {
+    id: 5,
+    name: "Pesa de 30Kg",
+    price: 50,
+    image: "images/product-5.jpg",
+    stock: 50,
+  },
+  {
+    id: 6,
+    name: "Bolsa de boxeo con pie",
+    price: 50,
+    image: "images/product-6.jpg",
+    stock: 50,
+  },
 ];
 
 app.get("/api/products", (req, res) => {
-  res.send(products);
-});
-
-app.post("/api/pay", (req, res) => {
-  const ids = req.body;
-  const procutsCopy = products.map((p) => ({ ...p }));
-  ids.forEach((id) => {
-    const product = procutsCopy.find((p) => p.id === id);
-    if (product.stock > 0) {
-      product.stock--;
-    } else {
-      throw "Sin stock";
-    }
-  });
-  products = procutsCopy;
   res.send(products);
 });
 
