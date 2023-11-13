41ec8d94ae9a65a025daae033f0483d5c9b3ef0a
maurocipriano19 <104109893+maurocipriano19@users.noreply.github.com>
2023-11-13 15:49:07 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,65 +1,34 @@
-let productList = [];
-let carrito = [];
+let products = [];
 let total = 0;
 
-function add(productId, price) {
-    const product = productList.find(p => p.id === productId);
-    product.stock--;
-
-
-    console.log(productId, price);
-    carrito.push(productId);
+function add(product, price) {
+    console.log(product, price);
+    products.push(product);
     total = total + price;
     document.getElementById("checkout").innerHTML = `Pagar $${total}`
-    displayProducts();
 }
 
-async function pay() {
-    try{
-        const productList = await (await fetch("/api/pay",{
-            method: "post",
-            body: JSON.stringify(carrito),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })).json();
-    }
-    catch {
-        window.alert("Sin stock");
-    }
-
-    carrito = [];
-    total = 0;
-    await fetchProducts();
-    document.getElementById("checkout").innerHTML = `Pagar $${total}`
+function pay() {
+    window.alert(products.join(", \n"));
 }
 
 //-----
-function displayProducts() {
+function displayProducts(productList) {
     let productsHTML = '';
-    productList.forEach(p => {
-        let buttonHTML = `<button class="button-add" onclick="add(${p.id}, ${p.price})">Agregar</button>`;
-
-        if (p.stock <= 0) {
-            buttonHTML = `<button disabled class="button-add disabled" onclick="add(${p.id}, ${p.price})">Sin stock</button>`;
-        }
-
+    productList.forEach(element => {
         productsHTML +=
         `<div class="product-container">
-            <h3>${p.name}</h3>
-            <img src="${p.image}" />
-            <h1>$${p.price}</h1>
-            ${buttonHTML}
+            <h3>${element.name}</h3>
+            <img src="${element.image}" />
+            <h1>$${element.price}</h1>
+            <button class="button-add" onclick="add(${element.id}, ${element.price})">Agregar</button>
         </div>`
     });
     document.getElementById('page-content').innerHTML = productsHTML;
 }
 
-async function fetchProducts(){
-    productList = await (await fetch("/api/products")).json();
-    displayProducts();
-}
-
 window.onload = async() => {
-    await fetchProducts();
+    const productList = await (await fetch("/api/products")).json();
+    console.log(productList);
+    displayProducts(productList);
 }
